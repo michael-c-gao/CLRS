210f0fef8c5faaa02b6e4b354fe8c80f249615dc
michael-c-gao <michaelgao16@gmail.com>
2021-07-31 00:22:36 -0700

    queue with node

---
+++ ch_10/.1/nodeQueue.py
@@ -0,0 +1,57 @@
+#queue implemented with nodes
+#FIFO/LILO
+
+class Node():
+
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Queue():
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def nq(self, x):
+        if self.isEmpty():
+            self.head = x
+        else:
+            self.tail.next = x
+        self.tail = x
+        self.size +=1
+        
+    def dq(self):
+        if self.isEmpty():
+            print("can't dequeue from an empty queue!")
+            return 0
+        else:
+            a = self.head
+            self.head = self.head.next
+            self.size -=1
+            return a
+
+    def isEmpty(self):
+        return self.size == 0
+
+
+def main():
+    a = Node("h")
+    b = Node("i")
+    c = Node("!")
+    q = Queue()
+    q.nq(a)
+    q.nq(b)
+    q.nq(c)
+    print(q.head.value)
+    print(q.head.next.value)
+    print(q.tail.value)
+    q.dq()
+    print("_____")
+    print(q.head.value)
+    
+
+
+if __name__ == "__main__":
+    main()
